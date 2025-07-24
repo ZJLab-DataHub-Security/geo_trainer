79323954ffea94744b5d4333be780991eda4f9c8
qianhao <qianhao@zhejianglab.com>
2025-07-24 02:14:39 +0000

    add qwen3_moe

---
+++ examples/qwen/qwen3_sft.sh
@@ -0,0 +1,551 @@
+#!/bin/bash
+set -eo pipefail 
+
+ENV=dsw
+ROOT_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
+ROOT_DIR=${ROOT_DIR}/../.. #v5000_megatron
+echo $ROOT_DIR
+
+MEGATRON_PATH=/workspace/Megatron-LM
+#export LD_LIBRARY_PATH=/mnt/v5000-megatron/v5000-megatron/liusong/output/so:$LD_LIBRARY_PATH
+export PYTHONPATH=${PYTHONPATH}:${ROOT_DIR}:${MEGATRON_PATH}
+
+### BASE CONFIG ###
+MODEL_SIZE=A3B
+BATCH_SIZE=1
+GLOBAL_BATCH_SIZE=${GLOBAL_BATCH_SIZE:-32}
+LR=1e-5
+MIN_LR=1e-6
+SEQ_LEN=${SEQ_LEN:-32768}
+PAD_LEN=${SEQ_LEN}
+PR=${PR:-bf16}
+### BASE CONFIG ###
+
+### PARALLEL / BOOL OPTION ###
+TP=${TP:-4}
+PP=${PP:-1}
+EP=${EP:-8}
+ETP=${ETP:-1}
+CP=1
+SP=true
+DO=true
+FL=true
+SFT=true
+# MP_PP0_LAYERS=0
+### PARALLEL / BOOL OPTION ###
+
+### OTHERS ###
+AC=${AC:-full}
+RECOMPUTE_METHOD=${RECOMPUTE_METHOD:-uniform}
+MP_AC_LAYERS=${MP_AC_LAYERS:-1}
+OPTIMIZER_OFFLOAD=${OPTIMIZER_OFFLOAD:-false}
+SAVE_INTERVAL=${SAVE_INTERVAL:-100}
+PRETRAIN_CHECKPOINT_PATH=${PRETRAIN_CHECKPOINT_PATH:-/mnt/zj-gpfs/home/qianhao/models/mcore_qwen3_a3b_tp4_ep8/}
+DATASET_PATH=${DATASET_PATH:-/mnt/zj-gpfs/home/qianhao/data/qwen3_8b_sample_long_sft_32k_pack_text_document}
+VALID_DATASET_PATH=${DATASET_PATH}
+
+MP_SFT_PACKING=true
+CPT_CONTINUE=false
+
+# the following two values will not be used when SFT is true
+TRAIN_SAMPLES=${TRAIN_SAMPLES:-785483} 
+###############################
+
+if [[ -z ${OUTPUT_DIR} ]];then
+    OUTPUT_BASEPATH=${ROOT_DIR}/output
+else
+    OUTPUT_BASEPATH=${OUTPUT_DIR}
+fi
+SAVE_CKPT=true
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export NCCL_IB_GID_INDEX=3
+if [ $ENV = dsw ]; then
+
+    export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-"0,1,2,3,4,5,6,7"}
+    GPUS_PER_NODE=`echo "$CUDA_VISIBLE_DEVICES" | awk -F, '{print NF}'`
+    # Change for multinode config
+    MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+    NODE_RANK=${RANK:-0}
+    if [ "$NODE_RANK" -eq 0 ] && [ $MASTER_ADDR = "localhost" ]; then
+            MASTER_ADDR=${POD_NAME}
+    fi
+    echo "MASTER_ADDR is ${MASTER_ADDR}"
+    NNODES=${WORLD_SIZE:-1}
+    GPUS_PER_NODE=${TQ_GPU_NUM:-8}
+    MASTER_PORT=${MASTER_PORT:-9988}
+
+elif [ $ENV = dlc ]; then
+NNODES=${WORLD_SIZE}
+NODE_RANK=${RANK:-0}
+GPUS_PER_NODE=${TQ_GPU_NUM}
+fi
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+
+if [ $FL = true ]; then
+    export NVTE_FLASH_ATTN=1 NVTE_FUSED_ATTN=0
+    attn_backend_option=" \
+        --attention-backend flash \
+    "
+elif [ $FL = false ]; then
+    export NVTE_FLASH_ATTN=0 NVTE_FUSED_ATTN=1
+    attn_backend_option=" \
+        --attention-backend fused \
+    "
+fi
+
+if [ $MODEL_SIZE = 0.6B ]; then
+    NUM_LAYERS=28
+    HIDDEN_SIZE=1024
+    NUM_ATTENTION_HEADS=16
+    INTERMEDIATE_SIZE=3072
+    NUM_KEY_VALUE_HEADS=8
+    MAX_POSITION_EMBEDDINGS=40960
+    EXTRA_VOCAB_SIZE=293
+    ROPE_THETA=1000000
+    RMS_NORM_EPS=1e-6
+    gqa_options=" \
+                --group-query-attention \
+                --num-query-groups ${NUM_KEY_VALUE_HEADS}"
+    
+    tie_option=""
+    moe_options=""
+elif [ $MODEL_SIZE = 1.7B ]; then
+    NUM_LAYERS=28
+    HIDDEN_SIZE=2048
+    NUM_ATTENTION_HEADS=16
+    INTERMEDIATE_SIZE=6144
+    NUM_KEY_VALUE_HEADS=8
+    MAX_POSITION_EMBEDDINGS=40960
+    EXTRA_VOCAB_SIZE=293
+    ROPE_THETA=1000000
+    RMS_NORM_EPS=1e-6
+    gqa_options=" \
+                --group-query-attention \
+                --num-query-groups ${NUM_KEY_VALUE_HEADS}"
+    
+    tie_option=""
+    moe_options=""
+elif [ $MODEL_SIZE = 4B ]; then
+    NUM_LAYERS=36
+    HIDDEN_SIZE=2560
+    NUM_ATTENTION_HEADS=32
+    INTERMEDIATE_SIZE=9728
+    NUM_KEY_VALUE_HEADS=8
+    MAX_POSITION_EMBEDDINGS=40960
+    EXTRA_VOCAB_SIZE=293
+    ROPE_THETA=1000000
+    RMS_NORM_EPS=1e-6
+    gqa_options=" \
+                --group-query-attention \
+                --num-query-groups ${NUM_KEY_VALUE_HEADS}"
+    
+    tie_option=""
+    moe_options=""
+elif [ $MODEL_SIZE = 8B ]; then
+    NUM_LAYERS=36
+    HIDDEN_SIZE=4096
+    NUM_ATTENTION_HEADS=32
+    INTERMEDIATE_SIZE=12288
+    NUM_KEY_VALUE_HEADS=8
+    MAX_POSITION_EMBEDDINGS=40960
+    EXTRA_VOCAB_SIZE=293
+    ROPE_THETA=1000000
+    RMS_NORM_EPS=1e-6
+    gqa_options=" \
+                --group-query-attention \
+                --num-query-groups ${NUM_KEY_VALUE_HEADS}"
+    
+    tie_option=" \
+            --untie-embeddings-and-output-weights \
+            "
+    moe_options=""
+elif [ $MODEL_SIZE = 14B ]; then 
+    NUM_LAYERS=40
+    HIDDEN_SIZE=5120
+    NUM_ATTENTION_HEADS=40
+    INTERMEDIATE_SIZE=17408
+    NUM_KEY_VALUE_HEADS=8
+    MAX_POSITION_EMBEDDINGS=40960
+    EXTRA_VOCAB_SIZE=293
+    ROPE_THETA=1000000
+    RMS_NORM_EPS=1e-6
+    gqa_options=" \
+                --group-query-attention \
+                --num-query-groups ${NUM_KEY_VALUE_HEADS}"
+    
+    tie_option=" \
+            --untie-embeddings-and-output-weights \
+            "
+
+    moe_options=""
+elif [ $MODEL_SIZE = 32B ]; then
+    NUM_LAYERS=64
+    HIDDEN_SIZE=5120
+    NUM_ATTENTION_HEADS=64
+    INTERMEDIATE_SIZE=25600
+    NUM_KEY_VALUE_HEADS=8
+    MAX_POSITION_EMBEDDINGS=40960
+    EXTRA_VOCAB_SIZE=293
+    ROPE_THETA=1000000
+    RMS_NORM_EPS=1e-6
+    gqa_options=" \
+                --group-query-attention \
+                --num-query-groups ${NUM_KEY_VALUE_HEADS}"
+    
+    tie_option=" \
+            --untie-embeddings-and-output-weights \
+            "
+
+    moe_options=""
+elif [ $MODEL_SIZE = A3B ]; then
+    HIDDEN_SIZE=2048
+    NUM_ATTENTION_HEADS=32
+    NUM_LAYERS=48
+    INTERMEDIATE_SIZE=6144
+    MOE_INTERMEDIATE_SIZE=768
+    MAX_POSITION_EMBEDDINGS=40960
+    EXTRA_VOCAB_SIZE=293
+    NUM_KEY_VALUE_HEADS=4
+    ROPE_THETA=1000000
+    NUM_EXPERTS=128
+    ROUTER_TOPK=8
+    RMS_NORM_EPS=1e-6
+
+    #--moe-grouped-gemm \
+    moe_options=" \
+        --moe-token-dispatcher-type alltoall \
+        --moe-router-topk ${ROUTER_TOPK} \
+        --num-experts ${NUM_EXPERTS} \
+        --expert-tensor-parallel-size ${ETP} \
+        --expert-model-parallel-size ${EP} \
+        --moe-ffn-hidden-size ${MOE_INTERMEDIATE_SIZE} \
+        --moe-router-load-balancing-type aux_loss \
+        --moe-aux-loss-coeff 0.001 \
+        --moe-layer-freq '([1]*48)' \
+        "
+
+    tie_option=" \
+            --untie-embeddings-and-output-weights \
+            "
+
+    gqa_options=" \
+                --group-query-attention \
+                --num-query-groups ${NUM_KEY_VALUE_HEADS}"
+elif [ $MODEL_SIZE = A22B ]; then
+    HIDDEN_SIZE=4096
+    NUM_ATTENTION_HEADS=64
+    NUM_LAYERS=94
+    INTERMEDIATE_SIZE=12288
+    MOE_INTERMEDIATE_SIZE=1536
+    MAX_POSITION_EMBEDDINGS=40960
+    EXTRA_VOCAB_SIZE=293
+    NUM_KEY_VALUE_HEADS=4
+    ROPE_THETA=1000000
+    NUM_EXPERTS=128
+    ROUTER_TOPK=8
+    RMS_NORM_EPS=1e-6
+
+    #--moe-grouped-gemm \
+    moe_options=" \
+        --moe-token-dispatcher-type alltoall \
+        --moe-router-topk ${ROUTER_TOPK} \
+        --num-experts ${NUM_EXPERTS} \
+        --expert-tensor-parallel-size ${ETP} \
+        --expert-model-parallel-size ${EP} \
+        --moe-ffn-hidden-size ${MOE_INTERMEDIATE_SIZE} \
+        --moe-router-load-balancing-type aux_loss \
+        --moe-aux-loss-coeff 0.001 \
+        --moe-layer-freq '([1]*94)' \
+        --moe-router-pre-softmax
+        "
+
+    tie_option=" \
+            --untie-embeddings-and-output-weights \
+            "
+
+    gqa_options=" \
+                --group-query-attention \
+                --num-query-groups ${NUM_KEY_VALUE_HEADS}"
+fi
+
+
+# Here are some configs controled by env
+if [ -z ${MP_DATASET_TYPE} ];then
+    MP_DATASET_TYPE="idxmap"
+fi
+
+if [ -z ${MP_AC_LAYERS} ];then
+    MP_AC_LAYERS=1
+fi
+
+if [ -z ${MP_VP} ]; then
+    vp_option=""
+else
+    vp_option=" \
+        --num-layers-per-virtual-pipeline-stage ${MP_VP}"
+fi
+
+if [ -z ${MP_SFT_PACKING} ]; then
+    MP_SFT_PACKING=false
+fi
+
+TP_COMM_OVERLAP=$(( ($TP > 1) ? 1 : 0 ))
+#comm_overlap_option="\
+#    --overlap-grad-reduce \
+#    --overlap-param-gather"
+
+comm_overlap_option=""
+#if [ $TP_COMM_OVERLAP -eq 1 ]; then
+#    comm_overlap_option="\
+#        --tp-comm-overlap \
+#        --overlap-grad-reduce \
+#        --overlap-param-gather"
+#fi
+
+if [ $AC = full ]; then
+    _check=$(( ($NUM_LAYERS / $PP) % ${MP_AC_LAYERS} ))
+    if [ $_check != 0 ]; then
+        echo "the num layers per pp rank must be a multiple of the recompute layers."
+    fi
+    activation_checkpoint_options=" \
+		    --recompute-method ${RECOMPUTE_METHOD} \
+            --recompute-num-layers ${MP_AC_LAYERS} \
+		    --recompute-granularity full"
+elif [ $AC = sel ]; then
+    activation_checkpoint_options=" \
+        --recompute-activations"
+elif [ $AC = none ]; then
+    activation_checkpoint_options=" \
+    "
+elif [ $AC = offload ]; then
+    activation_checkpoint_options=" \
+		    --cpu-offloading \
+		    --cpu-offloading-num-layers ${MP_AC_LAYERS}"
+    if [ $TP_COMM_OVERLAP -eq 1 ]; then
+        echo "Disable --overlap-grad-reduce and --overlap-param-gather when cpu offloading is on..."
+        comm_overlap_option="\
+            --tp-comm-overlap"
+    else
+        echo "Disable --overlap-grad-reduce and --overlap-param-gather when cpu offloading is on..."
+        comm_overlap_option=""
+    fi
+fi
+
+if [ $PR = fp16 ]; then
+    pr_options=" \
+		    --fp16 \
+            --apply-query-key-layer-scaling"
+    export NVTE_APPLY_QK_LAYER_SCALING=1
+elif [ $PR = bf16 ]; then
+    pr_options=" \
+        --bf16"
+elif [ $PR = fp8 ]; then
+    pr_options=" \
+        --bf16 \
+        --fp8-format hybrid \
+        --fp8-amax-compute-algo max \
+        --fp8-amax-history-len 1024"
+fi
+
+if [ $OPTIMIZER_OFFLOAD != false ] && [ $DO = false ]; then
+    echo "Offload optimizer is valid only if \$DO=true"
+    DO=true
+fi
+
+if [ $DO = true ]; then
+    do_option=" \
+		    --use-distributed-optimizer"
+
+elif [ $DO = false ]; then
+    do_option=" \
+                    "
+fi
+
+
+if [ $SP = true ] && [ $TP -gt 1 ]; then
+    sp_option=" \
+		    --sequence-parallel"
+
+elif [ $SP = false ]; then
+    sp_option=" \
+                    "
+fi
+
+if [ -z ${MP_PP0_LAYERS} ];then
+    uneven_split_option=""
+elif [ ${PP} -gt 1 ]; then
+    _check=$(( ( $NUM_LAYERS - ${MP_PP0_LAYERS} ) % ( ${PP} - 1 ) ))
+    if [ $_check != 0 ]; then
+        echo "With uneven pipelineing the left over layers must be divisible by left over stages."
+        exit -1
+    fi
+
+    uneven_split_option=" \
+        --decoder-first-pipeline-num-layers ${MP_PP0_LAYERS}"
+else
+    echo "uneven pipeline split must be used when PP > 1"
+    exit -1
+fi
+
+if [ $PRETRAIN_CHECKPOINT_PATH != none ]; then
+    load_option=" \
+            --load $PRETRAIN_CHECKPOINT_PATH"
+fi
+
+if [ $OPTIMIZER_OFFLOAD != false ]; then
+    offload_option=" \
+        --optimizer-cpu-offload \
+        --use-precision-aware-optimizer \
+        --optimizer-offload-fraction ${OPTIMIZER_OFFLOAD}"
+fi
+
+if [ $SFT = true ]; then
+    TASK="sft"
+    sft_options=" \
+         --eod-mask-loss \
+         --calculate-per-token-loss \
+         --train-mode finetune"
+else
+    TASK="pretrain"
+    sft_options=" \
+        --train-mode pretrain"
+fi
+
+if [ ${MP_DATASET_TYPE} = "raw" ]; then
+    dataset_options=" \
+        --train-data-path ${DATASET_PATH} \
+        --valid-data-path ${VALID_DATASET_PATH} \
+        --dataloader-type cyclic \
+        --dataset JSON-SFT "
+else 
+    dataset_options=" \
+        --data-path ${DATASET_PATH} \
+        --dataset MMAP \
+        --split 99,1,0 "
+fi
+
+if [ ${MP_SFT_PACKING} = true ]; then
+    packing_options=" \
+      --reset-position-ids \
+      --no-create-attention-mask-in-dataloader "
+else
+    packing_options=""
+fi
+
+##### Prepare logdirs #######
+CURRENT_TIME=$(date +"%m-%d-%H:%M")
+TASK_NAME="mcore-qwen3-${MODEL_SIZE}-${TASK}"
+DETAIL_TASK_NAME="${TASK_NAME}-lr-${LR}-minlr-${MIN_LR}-bs-${BATCH_SIZE}-gbs-${GLOBAL_BATCH_SIZE}-seqlen-${SEQ_LEN}-pr-${PR}-tp-${TP}-pp-${PP}-cp-${CP}-ac-${AC}-do-${DO}-sp-${SP}/${CURRENT_TIME}${LABEL}"
+TENSORBOARD_DIR="${OUTPUT_BASEPATH}/${DETAIL_TASK_NAME}"
+SAVED_PRETRAIN_CHECKPOINT_PATH="${OUTPUT_BASEPATH}/checkpoint/${TASK_NAME}"
+LOG_DIR=${OUTPUT_BASEPATH}/${DETAIL_TASK_NAME}
+LOG_NAME="${NODE_RANK}.txt"
+
+mkdir -p ${SAVED_PRETRAIN_CHECKPOINT_PATH}
+mkdir -p ${LOG_DIR}
+mkdir -p ${TENSORBOARD_DIR}
+
+if [ $SAVE_CKPT = true ]; then
+    save_ckpt_options=" \
+        --save ${SAVED_PRETRAIN_CHECKPOINT_PATH} \
+        --save-interval ${SAVE_INTERVAL} \
+        --ckpt-format torch_dist "
+fi
+
+if [ -z ${CPT_CONTINUE} ] || [ ${CPT_CONTINUE} = false ]; then
+    cpt_continue_options="\
+     --no-load-optim \
+     --no-load-rng "
+elif [ ${CPT_CONTINUE} = true ];  then
+    cpt_continue_options="\
+        --no-load-rng "
+fi
+
+find -L ${PRETRAIN_CHECKPOINT_PATH} -maxdepth 1 -type f -name "*.json" -print0 | xargs -0 cp -t ${SAVED_PRETRAIN_CHECKPOINT_PATH}
+
+
+megatron_options="  \
+        --lr ${LR} \
+        --min-lr ${MIN_LR} \
+        --lr-decay-style cosine \
+        --weight-decay 0.01 \
+        --adam-beta1 0.9 \
+        --adam-beta2 0.95 \
+        --clip-grad 1.0 \
+        --init-method-std 0.008 \
+        --attention-dropout 0.0 \
+        --hidden-dropout 0.0 \
+        --lr-warmup-fraction 0.1 \
+        --train-samples ${TRAIN_SAMPLES}
+        --micro-batch-size ${BATCH_SIZE} \
+        --global-batch-size ${GLOBAL_BATCH_SIZE} \
+        --num-layers ${NUM_LAYERS} \
+        --hidden-size ${HIDDEN_SIZE} \
+        --num-attention-heads ${NUM_ATTENTION_HEADS} \
+        --ffn-hidden-size ${INTERMEDIATE_SIZE} \
+        --seq-length ${SEQ_LEN} \
+        --max-position-embeddings ${MAX_POSITION_EMBEDDINGS} \
+        --max-padding-length ${PAD_LEN} \
+        --log-interval 1 \
+        --log-throughput \
+        --eval-interval 10000 \
+        --eval-iters 0 \
+        --tensorboard-queue-size 1 \
+        --tensorboard-dir ${TENSORBOARD_DIR} \
+        --log-timers-to-tensorboard \
+        --log-validation-ppl-to-tensorboard \
+        --tensor-model-parallel-size ${TP} \
+        --pipeline-model-parallel-size ${PP} \
+        --context-parallel-size ${CP} \
+        --num-workers 8 \
+        --extra-vocab-size ${EXTRA_VOCAB_SIZE} \
+        --patch-tokenizer-type Qwen3Tokenizer \
+        --swiglu \
+        --normalization RMSNorm \
+        --norm-epsilon ${RMS_NORM_EPS} \
+        --use-rotary-position-embeddings \
+        --position-embedding-type rope \
+        --disable-bias-linear \
+        --rotary-base ${ROPE_THETA} \
+        --no-save-optim \
+        --no-rope-fusion \
+        --moe-token-dispatcher-type alltoall \
+        --ckpt-format torch_dist \
+        --transformer-impl transformer_engine \
+        --cross-entropy-loss-fusion \
+        --qk-layernorm \
+        --kv-channels 128 \
+        --use-cpu-initialization \
+        "
+
+if [[ -z ${LOG_FILE} ]];then
+  LOG_FILE=${LOG_DIR}/${LOG_NAME}
+fi
+
+run_cmd="torchrun $DISTRIBUTED_ARGS run_qwen3.py
+ ${megatron_options} \
+ ${dataset_options} \
+ ${pr_options} \
+ ${load_option} \
+ ${activation_checkpoint_options} \
+ ${do_option} \
+ ${sp_option} \
+ ${moe_options} \
+ ${offload_option} \
+ ${sft_options} \
+ ${vp_option} \
+ ${packing_options} \
+ ${uneven_split_option} \
+ ${attn_backend_option} \
+ ${cpt_continue_options} \
+ ${save_ckpt_options} \
+ ${gqa_options} \
+ ${tie_option} \
+ 2>&1 | tee ${LOG_FILE}
+ "
+
+echo ${run_cmd}
+eval ${run_cmd}
+set +x
--- megatron_patch/data/__init__.py
+++ megatron_patch/data/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Alibaba PAI Team.
+# Copyright (c) 2025 Alibaba PAI Team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,51 +14,115 @@
 
 import numpy as np
 try:
-    from megatron import get_args
+    # <= 240126
+    from megatron import (
+        get_args, 
+        print_rank_0,
+    )
 except:
-    from megatron.training import get_args
+    # >= 240405
+    from megatron.training import (
+        get_args,
+        print_rank_0
+    )
 
-from megatron_patch.tokenizer import build_tokenizer
-from .llama import LLamaRawDataset, LLamaSFTDataset
+from megatron.core import mpu
+from megatron_patch.tokenizer import build_tokenizer, get_tokenizer
+from .json_sft import JSONSFTDataset
 
 def build_evaluation_dataset(dataset):
-
-    args = get_args()
-    build_tokenizer(args)
-    if dataset == 'LLama-SFT' or dataset == 'LLama-Pretrain-Raw':
-        val_dataset = LLamaRawDataset(args.valid_data_path, args.max_padding_length)
-        return val_dataset
-    else:
-        raise NotImplementedError('dataset {} is not implemented.'.format(dataset))
+    raise NotImplementedError(f"Dataset {dataset} is no longer supported in Pai-Megatron-Patch anymore, downgrade to v0.10.2 or lower to use it.")
 
 def build_finetune_dataset(dataset):
+    raise NotImplementedError(f"Dataset {dataset} is no longer supported in Pai-Megatron-Patch anymore, downgrade to v0.10.2 or lower to use it.")
 
+# NOTE: better to use shared train_valid_test_datasets_provider instead of build_dataset
+def train_valid_test_datasets_provider(train_val_test_num_samples):
+    """Build the train test and validation datasets.
+
+    Args:
+        train_val_test_num_samples : A list containing the number of samples in train test and validation.
+    """
     args = get_args()
-    build_tokenizer(args)
-    if dataset == 'LLama-SFT':
-        train_dataset = LLamaRawDataset(args.train_data_path, args.max_padding_length)
-        valid_dataset = LLamaRawDataset(args.valid_data_path, args.max_padding_length)
+    if get_tokenizer() is None:
+        build_tokenizer(args)
+    print_rank_0("> building train, validation, and test datasets for GPT ...")
+    return build_dataset(args, train_val_test_num_samples)
 
-        return train_dataset, valid_dataset
+def core_gpt_dataset_config_from_args(args):
+    """
+        NOTE: require >= 240405
+    """
+    from megatron.core.datasets.gpt_dataset import GPTDatasetConfig
+    from megatron.core.datasets.utils import get_blend_from_list
+    tokenizer = get_tokenizer()
+    kwargs =dict(
+        random_seed=args.seed,
+        sequence_length=args.seq_length,
+        blend=get_blend_from_list(args.data_path),
+        blend_per_split=[
+            get_blend_from_list(args.train_data_path),
+            get_blend_from_list(args.valid_data_path),
+            get_blend_from_list(args.test_data_path),
+        ],
+        split=args.split,
+        path_to_cache=args.data_cache_path,
+        reset_position_ids=args.reset_position_ids,
+        reset_attention_mask=args.reset_attention_mask,
+        eod_mask_loss=args.eod_mask_loss,
+        mmap_bin_files=args.mmap_bin_files,
+        tokenizer=tokenizer,
+        create_attention_mask=args.create_attention_mask_in_dataloader,
+    )
+    try:
+        return GPTDatasetConfig(
+            num_dataset_builder_threads=args.num_dataset_builder_threads, 
+            **kwargs
+        )
+    except Exception:
+        # 240405
+        return GPTDatasetConfig(**kwargs)
+
+def is_dataset_built_on_rank():
+    return (
+        mpu.is_pipeline_first_stage() or mpu.is_pipeline_last_stage()
+    ) and mpu.get_tensor_model_parallel_rank() == 0
+
+
+def is_dataset_built_on_rank_packing():
+    return mpu.get_tensor_model_parallel_rank() == 0
+
+
+def build_dataset(args, train_val_test_num_samples):
+    from megatron.core.datasets.gpt_dataset import (
+        GPTDataset,
+        MockGPTDataset,
+    )
+    from megatron.core.datasets.blended_megatron_dataset_builder import (
+        BlendedMegatronDatasetBuilder,
+    )
+    if get_tokenizer() is None:
+        build_tokenizer(args)
+    if args.dataset == 'JSON-SFT':
+        train_dataset = JSONSFTDataset(args.train_data_path, args.max_padding_length)
+        val_dataset = JSONSFTDataset(args.valid_data_path, args.max_padding_length)
+        test_dataset = JSONSFTDataset(args.valid_data_path, args.max_padding_length)
+    elif args.dataset == 'MMAP':
+        config = core_gpt_dataset_config_from_args(args)
+        dataset_type = MockGPTDataset if config.mock else GPTDataset
+        should_build_dataset = is_dataset_built_on_rank
+        if args.train_mode != "pretrain":
+            # NOTE: in data preparation scripts, the sequence is collect into (seq, labels)
+            # therefore we need to double the seqlen
+            config.sequence_length = config.sequence_length * 2
+            if args.reset_position_ids:
+                should_build_dataset = is_dataset_built_on_rank_packing
+
+        train_dataset, val_dataset, test_dataset = BlendedMegatronDatasetBuilder(
+            dataset_type, train_val_test_num_samples, should_build_dataset, config
+        ).build()
+        print_rank_0("> finished creating GPT datasets ...")
     else:
-        raise NotImplementedError('dataset {} is not implemented.'.format(dataset))
-
-def build_pretrain_dataset_from_original(dataset):
-
-    args = get_args()
-    build_tokenizer(args)
-    if dataset == 'LLama-Pretrain-Raw':
-        train_dataset = LLamaRawDataset(args.train_data_path, args.max_padding_length)
-        #valid_dataset = LLamaRawDataset(args.valid_data_path, args.max_padding_length)
-        #test_dataset = LLamaRawDataset(args.test_data_path, args.max_padding_length)
-        # customize your validation and test dataset here
-
-        return train_dataset, train_dataset, train_dataset
-
-
-    elif dataset == 'LLama-SFT-Raw':
-        train_dataset = LLamaSFTDataset(args.train_data_path, args.max_padding_length)
-
-        return train_dataset, train_dataset, train_dataset
-    else:
-        raise NotImplementedError('dataset {} is not implemented.'.format(dataset))
+        raise NotImplementedError(f"Dataset {args.dataset} is no longer supported in Pai-Megatron-Patch anymore, downgrade to v0.10.2 or lower to use it.")
+    
+    return train_dataset, val_dataset, test_dataset
